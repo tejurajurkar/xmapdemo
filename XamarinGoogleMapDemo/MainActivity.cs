a3101a3cb0e70d79c079de230f945da6bed392f9
tejurajurkar <tejurajurkar@gmail.com>
2020-07-10 22:22:37 +0530

    first commit

---
+++ XamarinGoogleMapDemo/MainActivity.cs
@@ -0,0 +1,36 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Android.Gms.Maps;
+using Android.Gms.Maps.Model;
+
+namespace XamarinGoogleMapDemo
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity,IOnMapReadyCallback
+    {
+        public void OnMapReady(GoogleMap googleMap)
+        {
+            MarkerOptions markerOptions = new MarkerOptions();
+            markerOptions.SetPosition(new LatLng(21.03, 76.06));
+            markerOptions.SetTitle("My Position");
+            googleMap.AddMarker(markerOptions);
+            //optional
+            googleMap.UiSettings.ZoomControlsEnabled = true;
+            googleMap.UiSettings.CompassEnabled = true;
+            googleMap.MoveCamera(CameraUpdateFactory.ZoomIn());
+        }
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            MapFragment mapFragment = (MapFragment)FragmentManager.FindFragmentById(Resource.Id.map);
+            mapFragment.GetMapAsync(this);
+
+        }
+    }
+}
